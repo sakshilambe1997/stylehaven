9e75f63351fabea59788ef20d2b413d2441323b4
sakshilambe1997 <sakshilambe1997@gmail.com>
2025-04-19 15:50:19 +0530

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css'
 import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
 
   return (
     <>
-       <Header/>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path={"/"} exact={true} element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+       
     </>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
  const Header = () => {
   return (
     <div>
       <header>
-        <div className='top-strip py-2'>
+        <div className='top-strip py-2 border-pink-500 border-b-[1px]'>
           <div className='container'>
             <div className='flex items-center justify-between'>
               <div className='col1 w-[50%]'>
-                <p className='text-[14px]'> Get up to 50% off new season styles, limited time</p>
+                <p className='text-[13px] font-[500]'> Get up to 50% off new season styles, limited time</p>
+              </div>
+
+              <div className='col2 flex items-center justify-end '>
+
+                <ul className='flex items-center '>
+                  <li className='list-none'>
+                    <Link to="help-center" className='text-[13px] link font-[500]  mx-[4px] transition'> Help Center</Link>
+
+                    <Link to="/order tracking" className='text-[13px] link font-[500] mx-[4px] transition'> Order Tracking</Link>
+                  </li>
+                </ul>
               </div>
             </div>
 
