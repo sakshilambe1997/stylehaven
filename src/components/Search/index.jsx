0de628cd0c63416365c8946d673725276fb89601
sakshilambe1997 <sakshilambe1997@gmail.com>
2025-04-26 15:32:59 +0530

    changes added to search box

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../Search/style.css"
+import { IoMdSearch } from "react-icons/io";
+import Button from '@mui/material/Button';
 
  const Search = () => {
   return (
@@ -7,6 +9,7 @@ import "../Search/style.css"
         <div className='searchBox w-[100%] h-[40px] bg-[#e5e5e5] rounded-[10px] relative px-2'>
           <input type='text' placeholder='Search for products..' className='w-full h-[35px] focus:outline-none p-2 text-[15px]' />
             
+          <Button className='!absolute top-[2px] right-[5px] z-50 !w-[35px] !min-w-[35px] h-[35px] !rounded-full !text-black'><IoMdSearch className='text-[#0000008f] text-[50px]' /></Button>  
         </div>
     </div>
   )
